c78ca256317ad77b83606bfa649b9cbae8281c78
Eliel de Souza Fariã <elielrj@gmail.com>
2023-09-22 10:03:50 -0300

    Removido o método não utilizada fromFirestore, e incluida o método fromMap.

--- lib/model/bo/produto/Produto.dart
+++ lib/model/bo/produto/Produto.dart
@@ -28,34 +28,21 @@ class Produto {
     this.listaDeVisualizacoes,
   });
 
-  factory Produto.fromFirestore(
-    DocumentSnapshot<Map<String, dynamic>> snapshot,
-    SnapshotOptions? optios,
-  ) {
-    final data = snapshot.data();
+  factory Produto.fromMap(Map<String, dynamic>? map) {
     return Produto(
-      codigo: data?['codigo'],
-      estoque: data?['estoque'],
-      nome: data?['nome'],
-      listaDeAvaliacoes: data?['lista_de_avaliacoes'] is Iterable ? List.from(data?['lista_de_avaliacoes']) : null,
-      listaDeArquivos: data?['lista_de_arquivos'] is Iterable ? List.from(data?['lista_de_arquivos']) : null,
-      listaDeCategorias: data?['lista_de_categorias'] is Iterable ? List.from(data?['lista_de_categorias']) : null,
-      listaDeCurtidas: data?['lista_de_curtidas'] is Iterable ? List.from(data?['lista_de_curtidas']) : null,
-      listaDeVisualizacoes:
-          data?['lista_de_visualizacoes'] is Iterable ? List.from(data?['lista_de_visualizacoes']) : null,
-    );
-  }
-
-  factory Produto.fromMap(Map<String, dynamic> map) {
-    return Produto(
-      codigo: map['codigo'],
-      estoque: map['estoque'],
-      nome: map['nome'],
-      listaDeAvaliacoes: map['lista_de_avaliacoes'] is Iterable ? List.from(map['lista_de_avaliacoes']) : null,
-      listaDeArquivos: map['lista_de_arquivos'] is Iterable ? List.from(map['lista_de_arquivos']) : null,
-      listaDeCategorias: map['lista_de_categorias'] is Iterable ? List.from(map['lista_de_categorias']) : null,
-      listaDeCurtidas: map['lista_de_curtidas'] is Iterable ? List.from(map['lista_de_curtidas']) : null,
-      listaDeVisualizacoes: map['lista_de_visualizacoes'] is Iterable ? List.from(map['lista_de_visualizacoes']) : null,
+      codigo: map?['codigo'],
+      estoque: map?['estoque'] is Map ? Estoque.fromMap(map?['estoque']) : null,
+      nome: map?['nome'],
+      //listaDeAvaliacoes: map?['lista_de_avaliacoes'] is Iterable ? List.from(map?['lista_de_avaliacoes']) : null,
+      listaDeArquivos: map?['lista_de_arquivos'] is Iterable
+          ? List.from(map!['lista_de_arquivos'])
+              .map((arquivo) => Arquivo.fromMap(arquivo))
+              .toList()
+              .cast()
+          : null,
+      //listaDeCategorias: map?['lista_de_categorias'] is Iterable ? List.from(map?['lista_de_categorias']) : null,
+      //listaDeCurtidas: map?['lista_de_curtidas'] is Iterable ? List.from(map?['lista_de_curtidas']) : null,
+      //listaDeVisualizacoes: map?['lista_de_visualizacoes'] is Iterable ? List.from(map?['lista_de_visualizacoes']) : null,
     );
   }
 
