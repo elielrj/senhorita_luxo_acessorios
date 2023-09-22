5f291bf3d8133f154b79bd5e4e297d2937b523dd
Eliel de Souza Fariã <elielrj@gmail.com>
2023-09-22 10:00:12 -0300

    Organização de Cores no App.

--- cores/cores.dart
+++
@@ -1,7 +0,0 @@
-
-
-import 'package:flutter/animation.dart';
-
-const COLOR_PRINCIPAL_SL = Color(0xFFFF9EB3);
-
-const COLOR_SECUNDARIA_SL = Color.fromRGBO(220, 237, 236, 1.0);
---
+++ lib/bibioteca/cores/cores.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+
+const corRosaPrincipalDaSenhoritaLuxoAcessorios = Color(0xFFFF9EB3);
+
+const corVerdeClaroSecundAriaDaSenhoritaLuxoAcessorios =
+    Color.fromRGBO(220, 237, 236, 1.0);
