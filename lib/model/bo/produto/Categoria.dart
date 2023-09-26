617943a322bffb493ae6b2c79e9ce63f0a7e75b5
Eliel de Souza Fariã <elielrj@gmail.com>
2023-09-25 21:51:49 -0300

    Criado um método map.

--- lib/model/bo/produto/Categoria.dart
+++ lib/model/bo/produto/Categoria.dart
@@ -1,11 +1,14 @@
-
 import 'package:senhorita_luxo_acessorios/model/bo/produto/Produto.dart';
 
-class Categoria{
-
+class Categoria {
   String? descricao;
   List<Produto>? listaDeProdutos;
 
   Categoria({this.descricao, this.listaDeProdutos});
 
+  factory Categoria.fromMap(Map<String, dynamic> map) {
+    return Categoria(
+      descricao: map['descricao'],
+    );
+  }
 }
