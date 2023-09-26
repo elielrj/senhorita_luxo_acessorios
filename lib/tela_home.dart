04fc42e85ca3dcb214e6486642af6c652be46b09
Eliel de Souza Fariã <elielrj@gmail.com>
2023-09-25 21:52:34 -0300

    Impementado lista de categorias na appbar e no drawer.

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:senhorita_luxo_acessorios/bibioteca/cores/cores.dart';
 import 'package:senhorita_luxo_acessorios/bibioteca/textos/textos.dart';
+import 'package:senhorita_luxo_acessorios/model/bo/produto/Categoria.dart';
 import 'package:senhorita_luxo_acessorios/tela_adicionar_produto.dart';
 import 'package:senhorita_luxo_acessorios/tela_de_produtos.dart';
 import 'package:senhorita_luxo_acessorios/tela_imagem_card_top.dart';
@@ -13,15 +16,63 @@ class TelaHome extends StatefulWidget {
 }
 
 class _TelaHomeState extends State<TelaHome> {
+  final _listaDeCategorias = <Categoria>[];
+
+  Future<void> _buscarCategorias() async {
+    await FirebaseFirestore.instance
+        .collection('categorias')
+        .get()
+        .then((QuerySnapshot querySnapshot) async {
+      for (QueryDocumentSnapshot query in querySnapshot.docs) {
+        final data = query.data() as Map<String, dynamic>;
+        _listaDeCategorias.add(Categoria.fromMap(data));
+      }
+    });
+    setState(() => _listaDeCategorias);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _buscarCategorias();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: corRosaPrincipalDaSenhoritaLuxoAcessorios,
       appBar: AppBar(
-        title: const Text(
-          textoSenhoritaLuxoAcessorios,
-          style: TextStyle(
-              color: corVerdeClaroSecundAriaDaSenhoritaLuxoAcessorios),
+        title: Row(
+          children: [
+            const Text(
+              textoSenhoritaLuxoAcessorios,
+              style: TextStyle(
+                  color: corVerdeClaroSecundAriaDaSenhoritaLuxoAcessorios),
+            ),
+            kIsWeb
+                ? Row(
+                    children: [
+                      const SizedBox(
+                        width: 32,
+                      ),
+                      for (Categoria categoria in _listaDeCategorias)
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              categoria.descricao.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  )
+                : Container(width: 0)
+          ],
         ),
         leading: Builder(builder: (context) {
           return IconButton(
@@ -29,9 +80,7 @@ class _TelaHomeState extends State<TelaHome> {
               Icons.menu,
               color: corVerdeClaroSecundAriaDaSenhoritaLuxoAcessorios,
             ),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
+            onPressed: () => Scaffold.of(context).openDrawer(),
             tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
           );
         }),
@@ -69,8 +118,9 @@ class _TelaHomeState extends State<TelaHome> {
             SizedBox(
               width: double.infinity,
               height: 200,
-              child: Container(color: corVerdeClaroSecundAriaDaSenhoritaLuxoAcessorios,
-              child: const TelaImagemCardTop(),
+              child: Container(
+                color: corVerdeClaroSecundAriaDaSenhoritaLuxoAcessorios,
+                child: const TelaImagemCardTop(),
               ),
             ),
             const SizedBox(
@@ -98,13 +148,20 @@ class _TelaHomeState extends State<TelaHome> {
               child: Text(textoCategorias),
             ),
             ListTile(
-              title: const Row(children: [
-                Icon(Icons.add, color: Colors.black),
-                Text(
-                  textoProduto,
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ]),
+              title: const Column(
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.add, color: Colors.black),
+                        Text(
+                          textoProduto,
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                      ]),
+                ],
+              ),
               onTap: () {
                 Navigator.push(
                     context,
@@ -112,6 +169,27 @@ class _TelaHomeState extends State<TelaHome> {
                         builder: (context) => const TelaAdicionarProduto()));
               },
             ),
+            for (Categoria categoria in _listaDeCategorias)
+              ListTile(
+                title: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text(
+                        categoria.descricao.toString(),
+                        textAlign: TextAlign.start,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                onTap: () {},
+              )
           ],
         ),
       ),
